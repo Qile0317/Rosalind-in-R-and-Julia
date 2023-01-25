c49bdff5de6f4aa47fa9d1ba18ef08d5513207d3
Qile <qile0317@gmail.com>
2023-01-26 00:56:23 +0100

    readjust

--- R_armory/ini.r
+++ R_armory/ini.r
@@ -2,7 +2,7 @@ library(hash)
 
 # creating my own hashing syntax,
 # though might be better to just let it take a list instead
-Dict <- function(keys, values){
+dict <- function(keys, values){
     output <- hash()
     for (i in 1:length(keys)){
         output[[keys[i]]] <- values[i]
@@ -10,7 +10,7 @@ Dict <- function(keys, values){
     return(output)
 }
 
-nt_dict <- Dict(c("A", "C", "G", "T"), c(1, 2, 3, 4))
+nt_dict <- dict(c("A", "C", "G", "T"), c(1, 2, 3, 4))
 
 ini <- function(seq){
     output <- c(0, 0, 0, 0)
@@ -21,6 +21,6 @@ ini <- function(seq){
     return(output)
 }
 
-#I/O
+# I/O
 input <- readLines("inputs/rosalind_ini.txt")
 print(ini(input)) # 205 212 204 233
